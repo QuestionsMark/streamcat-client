728cbe04f8c4935989beba790da97cdc683880eb
Sławomir Dziurman <g1.slawomir.dziurman@gmail.com>
2023-01-30 18:37:40 +0100

    Section component updated

--- src/components/common/Section.tsx
+++ src/components/common/Section.tsx
@@ -7,13 +7,14 @@ interface Props extends HTMLAttributes<HTMLElement> {
     noPadding?: boolean;
     title?: string;
     titleClassName?: string;
+    contentClassName?: string;
 }
 
-export const Section = ({ children, center, className, flex, noPadding, title, titleClassName, ...rest }: Props) => {
+export const Section = ({ children, center, className, contentClassName, flex, noPadding, title, titleClassName, ...rest }: Props) => {
     return (
         <section {...rest} className={`section${title ? ' section--title' : ''}${className ? ' ' + className : ''}`}>
             {title && <h2 className={`section__title${titleClassName ? ' ' + titleClassName : ''}`}>{title}</h2>}
-            <div className={`section__content${flex ? ' section__content--flex' : ''}${center ? ' section__content--center' : ''}${noPadding ? ' section__content--no-padding' : ''}`}>
+            <div className={`section__content${flex ? ' section__content--flex' : ''}${center ? ' section__content--center' : ''}${noPadding ? ' section__content--no-padding' : ''}${contentClassName ? ' ' + contentClassName : ''}`}>
                 {children}
             </div>
         </section>
